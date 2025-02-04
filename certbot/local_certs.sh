29f79434e78c7a32f6f1055d37e77573b764e690
geo_link <geo_link>
2025-02-04 22:08:49 +0000

    added docker-compose and fixed cert path

--- certbot/local_certs.sh
+++ certbot/local_certs.sh
@@ -1,1 +1,1 @@
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048     -keyout certbot/conf/live/localhost/privkey.pem     -out certbot/conf/live/localhost/fullchain.pem     -subj "/CN=localhost"
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048     -keyout conf/live/localhost/privkey.pem     -out conf/live/localhost/fullchain.pem     -subj "/CN=localhost"
