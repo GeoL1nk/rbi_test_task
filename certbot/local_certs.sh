57172e1615b356452c1862c3c2fbe4fe479232a5
geo_link <geo_link>
2025-02-06 20:44:37 +0000

    fixed certs issue

--- certbot/local_certs.sh
+++ certbot/local_certs.sh
@@ -1,1 +1,2 @@
+mkdir -p conf/live/localhost
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048     -keyout conf/live/localhost/privkey.pem     -out conf/live/localhost/fullchain.pem     -subj "/CN=localhost"
